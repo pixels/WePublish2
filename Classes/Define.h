6cb266385f56b1babbe656c0f200312a5cfe2cdc
Yusuke Kikkawa <kikkawa@yuimaar.com>
2010-09-17 21:49:54 +0900

    Merge karatsu zip. version 100917

--- Classes/Define.h
+++ Classes/Define.h
@@ -12,7 +12,7 @@
 #define USE_WEBKIT false
 #define PAGING_BY_TAP true
 #define PAGING_BY_BUTTON true
-#define TOP_ALIGN_ON_ZOOM true
+#define TOP_ALIGN_ON_ZOOM false
 
 // Page size
 #define WINDOW_AW 320
